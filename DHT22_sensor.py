c69cb77162dd9290e7b90220234d88d064e873d2
Quantaqua <111180425+Quantaqua@users.noreply.github.com>
2022-08-28 16:42:57 +0200

    Create DHT22_sensor.py

---
+++ DHT22_sensor.py
@@ -0,0 +1,13 @@
+import os
+import time
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+def DHT22():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C    Humidity={1:0.1f}%".format(temperature-0.55,humidity+0.1))
+    else:
+        print("Failed to retrieve data from humidity sensor")
